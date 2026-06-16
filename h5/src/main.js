2ed527a9ce1a96b772106eb7317cc59d72230b50
WuXianHotel <3801225958@qq.com>
2026-06-17 01:37:00 +0800

    chore: 开启vConsole

--- h5/src/main.js
+++ h5/src/main.js
@@ -9,7 +9,7 @@ import { initAuditMode } from './utils/audit.js';
 initAuditMode();
 
 // vConsole 调试面板开关（需要时改为 true）
-const ENABLE_VCONSOLE = false;
+const ENABLE_VCONSOLE = true;
 
 if (ENABLE_VCONSOLE) {
   // 启用方式（任一即可）：
